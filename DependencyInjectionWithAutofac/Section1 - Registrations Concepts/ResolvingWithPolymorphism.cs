fa9c37efa0946644343996c672e41966e409f9c3
Shahar Shokrani <shokrano@gmail.com>
2019-05-01 13:52:43 +0300

    Adding the output result

--- DependencyInjectionWithAutofac/Section1 - Registrations Concepts/ResolvingWithPolymorphism.cs
+++ DependencyInjectionWithAutofac/Section1 - Registrations Concepts/ResolvingWithPolymorphism.cs
@@ -11,6 +11,24 @@ namespace DependencyInjectionWithAutofac
     [TestClass]
     public class Section3
     {
+        [TestMethod]
+        public void ResolvingWithPolymorphism()
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<A>();
+            cb.Register<B>((c, p) => new B(p.TypedAs<C>()));
+            using (var c = cb.Build())
+            {
+                B myB = c.Resolve<B>(TypedParameter.From(new C()));
+
+                A myA = c.Resolve<A>(TypedParameter.From(myB));
+
+                Console.Write(myA);
+
+                //OUTPUT: I'm A with B of I'm B with C of I'm C
+            }
+        }
+
         private class C
         {
             public override string ToString()
@@ -45,23 +63,6 @@ namespace DependencyInjectionWithAutofac
             public override string ToString()
             {
                 return $"I'm A with B of {this.B}";
-            }
-        }
-
-
-        [TestMethod]
-        public void ResolvingWithPolymorphism()
-        {
-            var cb = new ContainerBuilder();
-            cb.RegisterType<A>();
-            cb.Register<B>((c, p) => new B(p.TypedAs<C>()));
-            using (var c = cb.Build())
-            {
-                B myB = c.Resolve<B>(TypedParameter.From(new C()));
-
-                A myA = c.Resolve<A>(TypedParameter.From(myB));
-
-                Console.Write(myA);
             }
         }
     }
