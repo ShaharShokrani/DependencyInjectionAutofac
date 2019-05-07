bfee27f54807509399eacf0654d898e9ccefd83b
Shahar Shokrani <shokrano@gmail.com>
2019-05-07 14:16:26 +0300

    Metadata Interrogation

---
+++ DependencyInjectionWithAutofac/Section4 - Implicit Relationship Types/MetadataInterrogation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Autofac;
+using Autofac.Core;
+using DependencyInjectionAutofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DependencyInjectionWithAutofac
+{    
+    public partial class Section4
+    {
+        //copy from section 4
+    }
+}
