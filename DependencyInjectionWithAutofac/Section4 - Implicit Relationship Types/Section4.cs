3b35defd07aac44d2a75bff29b4cede935757191
Shahar Shokrani <shokrano@gmail.com>
2019-05-07 13:56:32 +0300

    DelayedInstantiation

--- DependencyInjectionWithAutofac/Section4 - Implicit Relationship Types/Section4.cs
+++ DependencyInjectionWithAutofac/Section4 - Implicit Relationship Types/Section4.cs
@@ -11,5 +11,39 @@ namespace DependencyInjectionWithAutofac
     [TestClass]
     public class Section4
     {
+        class DelayedInstantiationReport
+        {
+            private Lazy<ConsoleLog> _log;
+
+            public DelayedInstantiationReport(Lazy<ConsoleLog> log)
+            {
+                this._log = log;
+                Console.WriteLine("Delayed Instantiation component created");
+            }
+
+            public void Report()
+            {
+                this._log.Value.Write("Log started");
+            }
+        }
+
+        [TestMethod]
+        public void DelayedInstantiation()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterType<ConsoleLog>();
+            builder.RegisterType<DelayedInstantiationReport>();
+
+            using (IContainer container = builder.Build())
+            {
+                DelayedInstantiationReport report = container.Resolve<DelayedInstantiationReport>();
+                report.Report();
+                
+                //OUTPUT:
+                //Delayed Instantiation component created
+                //Log started
+            }
+        }
     }
 }
